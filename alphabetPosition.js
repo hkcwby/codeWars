aeda7ff04337c17a2b97c37b49cca35ad28d5ebb
hkcwby <alexanderstevenson@me.com>
2023-02-06 15:23:36 -0600

    clean up console log statements from debug and added comments

--- alphabetPosition.js
+++ alphabetPosition.js
@@ -1,15 +1,18 @@
 function alphabetPosition(text) {
+  //force all the text to lower case
   const lowerText = text.toLowerCase();
+  //creating an alphabet string that can then be indexed for positional value
   const alphabet = "abcdefghijklmnopqrstuvwxyz";
+  //create an array to store positional data
   let output = [];
+  //traverse the text string
   for (let i = 0; i < text.length; i++) {
-    console.log(lowerText[i]);
+    //ignore all values not in the alphabet which are returned as -1 from indexOf and store in the output array
     if (alphabet.indexOf(lowerText[i]) >= 0)
       output.push(alphabet.indexOf(lowerText[i]) + 1);
   }
+  //convert the output array to string and replace the comma seperation with blank space
   text = output.toString().replaceAll(",", " ");
-  console.log(output);
-  console.log(text);
   return text;
 }
 
