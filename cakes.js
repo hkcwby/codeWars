0dfc2079344722653de33ea304f2b003db038be8
hkcwby <alexanderstevenson@me.com>
2023-02-09 16:56:51 -0600

    updated with comments and added a refactored solution

--- cakes.js
+++ cakes.js
@@ -14,20 +14,29 @@
 // cakes({apples: 3, flour: 300, sugar: 150, milk: 100, oil: 100}, {sugar: 500, flour: 2000, milk: 2000});
 
 function cakes(recipe, available) {
+  //for each recipe item check it is available then divide the availability by recipe amount(rounding down)
   for (key in recipe) {
     recipe[key] = available[key] ? Math.floor(available[key] / recipe[key]) : 0;
   }
-
+  //return the amount based on the limitation of the most scarce ingredient
   return Object.values(recipe).reduce((a, b) => Math.min(a, b));
 }
+
+// a refactored solution using destructuring to a single line solution.
+// >> 0 shift operator works effectively like math.floor by truncating the number to 32bit integer
+function refactoredCakes(recipe, available) {
+  return Math.min(
+    ...Object.keys(recipe).map((key) => (available[key] / recipe[key]) >> 0)
+  );
+}
 console.log(
-  cakes(
+  refactoredCakes(
     { flour: 500, sugar: 200, eggs: 1 },
     { flour: 1200, sugar: 1200, eggs: 5, milk: 200 }
   )
 );
 console.log(
-  cakes(
+  refactoredCakes(
     { apples: 3, flour: 300, sugar: 150, milk: 100, oil: 100 },
     { sugar: 500, flour: 2000, milk: 2000 }
   )
