fda78b3140a915de6937cfbebba220fa66f0aae9
hkcwby <alexanderstevenson@me.com>
2023-02-14 14:06:01 -0600

    completed another version using recursion to search within the range of the first solution found for an earlier solution, still doesnt beat required time complexity for the problem

--- sumOfPairs.js
+++ sumOfPairs.js
@@ -6,33 +6,55 @@
 // == [3, 7]
 
 // function sumPairs(ints, s) {
-//   const negative = ints.map((int) => s - int);
-//   console.log(negative);
-//   const check = ints
-//     .map((int) => negative.find((value) => value === int))
-//     .filter((value) => value != undefined);
-//   console.log(check);
-//   return check.length > 0 ? [check[0], s - check[0]] : undefined;
+//   const output = ints
+//     .map((int, index) => [
+//       int,
+//       ints.slice(index + 1).find((value) => value == s - int),
+//       ints
+//         .slice(index + 1)
+//         .indexOf(ints.slice(index + 1).find((value) => value == s - int)) +
+//         index +
+//         1,
+//     ])
+//     .filter((value) => !isNaN(value[1]))
+//     .reduce((a, b) => (a[2] <= b[2] ? a : b), [0, 0, ints.length]);
+//   return output[2] < ints.length ? output.slice(0, 2) : undefined;
 // }
 
 function sumPairs(ints, s) {
-  const output = ints
-    .map((int, index) => [
-      int,
-      ints.slice(index + 1).find((value) => value == s - int),
-      ints
-        .slice(index + 1)
-        .indexOf(ints.slice(index + 1).find((value) => value == s - int)) +
-        index +
-        1,
-    ])
-    .filter((value) => !isNaN(value[1]))
-    .reduce((a, b) => (a[2] <= b[2] ? a : b), [0, 0, ints.length]);
-
-  return output[2] < ints.length ? output.slice(0, 2) : undefined;
+  function helper(range, t, stored) {
+    // console.log("new helper called");
+    if (range.length <= 1) return stored;
+    let i = 0;
+    for (value of range) {
+      console.log("value", value);
+      console.log("t", t);
+      console.log("stored", stored);
+      console.log("range slice", range.slice(i + 1));
+      // console.log(
+      //   "check",
+      //   range.slice(i + 1).find((item) => item == t - value)
+      // );
+      if (!isNaN(range.slice(i + 1).find((item) => item == t - value))) {
+        // console.log(true);
+        // console.log(
+        //   "this is range index",
+        //   range.slice(i + 1, i + 1 + range.slice(i + 1).indexOf(t - value))
+        // );
+        // console.log("this is stored", [value, t - value]);
+        return helper(
+          range.slice(i + 1, i + 1 + range.slice(i + 1).indexOf(t - value)),
+          t,
+          [value, t - value]
+        );
+      }
+      i++;
+    }
+    return stored;
+  }
+  return helper(ints, s, undefined);
 }
-
 console.log(sumPairs([10, 5, 2, 3, 7, 5], 10));
-console.log(sumPairs([1, -2, 3, 0, -6, 1], -6));
+// console.log(sumPairs([1, -2, 3, 0, -6, 1], -6));
 
-console.log(sumPairs([0, 2, 0], 0));
+// console.log(sumPairs([0, 2, 0], 0));
