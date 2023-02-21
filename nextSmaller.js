da9565dba6cb853cda4c784ba745386f956dfd2b
hkcwby <alexanderstevenson@me.com>
2023-02-21 12:28:46 -0600

    added comments to better outline the process

--- nextSmaller.js
+++ nextSmaller.js
@@ -8,16 +8,19 @@
 // nextSmaller(2071) == 2017
 
 function nextSmaller(n) {
+  //stringify the number for ease of spliting and reversing
   const process = String(n).split("").reverse();
+  //we want to check through the values in reverse order to find the first opportunity for a modifcation
   const check = process
     .map((item, index) => process[index] < process[index + 1])
     .indexOf(true);
-
+  //if the check yields no possible swappable values we return -1
   if (check < 0) return -1;
-
+  //splice the relevant range we want to modify only keep the remainder in the original array to reattach later
   const modify = process.splice(0, check + 2);
+  //the turningPoint is the value the that must be shifted
   const turningPoint = modify[modify.length - 1];
-
+  //search for the closest value that is less than the turning point value
   const best = modify.indexOf(
     String(
       modify
@@ -26,16 +29,15 @@ function nextSmaller(n) {
         .reduce((a, b) => Math.max(a, b), -10) + Number(turningPoint)
     )
   );
-  console.log(modify);
-  console.log(best);
 
+  //take the best candidate value
   const lead = modify.splice(best, 1);
-
+  //reconstruct our new lower number from the components sorting the remaing values high to low
   const output = process
     .reverse()
     .concat(lead, modify.sort().reverse())
     .join("");
-
+  //check the new leading number is not 0 because otherwise we will not be returning all digits
   return Number(output[0]) ? Number(output) : -1;
 }
 
@@ -43,5 +45,5 @@ function nextSmaller(n) {
 //console.log(nextSmaller(907));
 // console.log(nextSmaller(531));
 // console.log(nextSmaller(414));
-console.log(nextSmaller(1207));
+//console.log(nextSmaller(1207));
 // nextSmaller(8454231);
